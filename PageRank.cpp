06a048ce3227d49336704f4db9f17eef1a15fba2
Anika Solapurkar <anikaas2@illinois.edu>
2022-12-07 23:09:47 -0600

    finished methods

--- PageRank.cpp
+++ PageRank.cpp
@@ -8,28 +8,27 @@
 vector<double> PageRank::vec_initial(){
     vector<double> init(this->num);
     for(int i = 0; i < this->num; i++){
-        initial[i] = rand();
+        init[i] = rand();
     }
     return init;
 }
 
-
 //adjusts the adjacency matrix 
-void PageRank::createadjacency(int n, double d) {
-    this->n = n;
+void PageRank::createadjacency(int num, double d) {
+    this->num = num;
     double factor = (1-d)/n;
-    for(int i = 0; i < n; i++){
+    for(int i = 0; i < num; i++){
         double sum = 0;
-        for(int j = 0; j < n; j++){
+        for(int j = 0; j < num; j++){
             sum += adjacency[j][i];
         }     
         if(sum == 0){
-            for(int j = 0; j < n; j++){
+            for(int j = 0; j < num; j++){
                 adjacency[j][i] = 1/(double)n;
             }              
         }
 
-        for(int j = 0; j < n; j++){
+        for(int j = 0; j < num; j++){
             adjacency[j][i] = (adjacency[j][i]/sum)*d+factor;
         }             
     }
@@ -37,8 +36,8 @@ void PageRank::createadjacency(int n, double d) {
 
 //perform pagerank algorithm and stores result in vector
 vector<double> PageRank::rank(vector<double> initial, int t, bool store) {
-    vector<double> vec = initial;
-    vector<double> vec2 = initial;
+    vector<double> vec = init;
+    vector<double> vec2 = init;
     for(int t = 0; t < t; t++){
         for(int i = 0; i < num; i++){
             vec2[i] = 0;
@@ -53,12 +52,91 @@ vector<double> PageRank::rank(vector<double> initial, int t, bool store) {
                         vec2[n] = vec2[n] / s;
                     }
                 }
-
             }     
         }
         vec = vec2;
     }
     page_rank = vec;
     return vec;
+}
+
+
+//pick out the most important airport
+vector<int> PageRank::top_airport(int num){
+    int pos = 0;
+    vector<double> biggest(num);
+    vector<int> airport(num);
+    for(int i = 0; i < num; i++){
+        double maximum = 0;
+        for(auto j = page_rank.begin(); j != page_rank.end(); j++){
+            if(maximum < i*){
+                bool val = true;
+                for(auto k = biggest.begin(); k != biggest.end(); ++k){
+                    if(*j == *k) {
+                        val = false;
+                        break;
+                    }
+                }
+                if(val)
+                    maximum = *i;
+            }
+        }
+        biggest[pos] = (maximum);
+        pos++;
+    }
+    pos = 0;
+    for(int i = 0; i < num; i++){
+        int counter = 0;
+        for(auto it = page_rank.begin(); it != page_rank.end(); it++){
+            if(biggest[i] == *it){
+                airport[pos] = (airport_id[counter]);
+                pos++;
+            }
+            counter++;
+        }
+    }
+    //printing out the top airport 
+    cout<<"top "; 
+    cout<<num; 
+    cout<<" important airport\n"; 
+    for(int i = 0; i < num; i++){
+        cout<<airport[i];
+        cout<<"\n";
+    }
+    return airport;
+}
+
+//prints out the results of page rank algorithm
+void PageRank::print_result(){
+    cout<<"page rank result\n"; 
+    cout<<" ID      value\n"; 
+    for(int i = 0; i < this->num; i++){
+        cout<<this->airport_id[i];
+        cout<<" ";
+        cout<<this->page_rank[i];
+        cout<<"\n";
+    }
+    cout<<"\n";
+}
+
+//prints out adjancency matrix 
+void PageRank::print_adjmatrix(){
+    cout<<"adj matrix"<<endl;
+    cout<<"      ";
+    for(int i = 0; i < this->num; i++){
+        cout<<this->airport_id[i];
+        cout<<"           ";
+    }
+    cout<<"\n";
+    for(int i = 0; i < this->num; i++){
+        cout<<this->airport_id[i];
+        cout<<"  ";
+        for(int j = 0; j < this->num; j++){
+            cout<<this->adjacency[i][j];
+            cout<<"  ";
+        } 
+        cout<<"\n";
+    }
+    cout<<"\n";
 }
 
--- PageRank.h
+++ PageRank.h
@@ -6,10 +6,14 @@ using namespace std;
 class PageRank {
     public:
     void createadjacency(int n double d); 
-    vector<double> rank(vector<double> initial, int time, bool normalize);
+    vector<double> rank(vector<double> init, int t, bool  store);
     vector<int> bestairport(int num);
     vector<double> vec_initial();
+    void print_adjmatrix();
+    void print_result();
+     vector<int> airport_id; 
     vector<vector<double>> adjacency; 
     vector<double> page_rank; 
+    int num;
 
 }
